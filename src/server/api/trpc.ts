2c66e38698935b25fecdfdb83f9ab50473102e85
n4ze3m <n4ze3m@gmail.com>
2023-04-10 20:43:00 +0530

    new changes

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -16,7 +16,7 @@
  */
 import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
 
-import { prisma } from "../../server/db";
+import { prisma } from "~/server/db";
 
 type CreateContextOptions = Record<string, never>;
 
