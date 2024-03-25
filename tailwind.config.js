628d877b852f3b0b2d1c891238c640a9bacf4470
n4ze3m <n4ze3m@gmail.com>
2024-03-25 23:48:45 +0530

    Refactor tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    mode: "jit",
-    darkMode: "class",
-    content: ["./src/**/*.tsx"],
-    plugins: [
-      require('@tailwindcss/forms'),
-      require('@tailwindcss/typography')
-    ]
-  }
+  mode: "jit",
+  darkMode: "class",
+  content: ["./src/**/*.tsx"],
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")]
+}
