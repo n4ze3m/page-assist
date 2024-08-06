f1214f314dbe34db8181c11b27bb0367bd16b798
Ased Mammad <mammad.ased@gmail.com>
2024-08-06 11:49:36 +0330

    i18n: Add persian (farsi)

---
+++ src/i18n/lang/fa.ts
@@ -0,0 +1,17 @@
+import option from "@/assets/locale/fa/option.json"
+import playground from "@/assets/locale/fa/playground.json"
+import common from "@/assets/locale/fa/common.json"
+import sidepanel from "@/assets/locale/fa/sidepanel.json"
+import settings from "@/assets/locale/fa/settings.json"
+import knowledge from "@/assets/locale/fa/knowledge.json"
+import chrome from "@/assets/locale/fa/chrome.json"
+
+export const fa = {
+    option,
+    playground,
+    common,
+    sidepanel,
+    settings,
+    knowledge,
+    chrome
+}
