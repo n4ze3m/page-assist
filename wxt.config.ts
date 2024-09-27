2e97f6470d64a815fb0181dd3253f5463ba9e69e
n4ze3m <n4ze3m@gmail.com>
2024-09-27 19:38:29 +0530

    feat: Add clear chat history button to drawer
    Adds a button to the sidebar drawer for clearing chat history.
    The button uses a confirmation prompt to ensure user intent and clears the database entries.
    This allows users to reset their chat history for a fresh start.
    The button uses a tooltip to provide more information on its function.

--- wxt.config.ts
+++ wxt.config.ts
@@ -50,7 +50,7 @@ export default defineConfig({
   outDir: "build",
 
   manifest: {
-    version: "1.2.3",
+    version: "1.2.4",
     name:
       process.env.TARGET === "firefox"
         ? "Page Assist - A Web UI for Local AI Models"
