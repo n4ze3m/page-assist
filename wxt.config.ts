9e4921583651de385247f2db55425d3b72fa24c3
n4ze3m <n4ze3m@gmail.com>
2024-10-27 00:53:21 +0530

    Bump version to 1.3.2
    Minor version bump to reflect the latest updates.

--- wxt.config.ts
+++ wxt.config.ts
@@ -50,7 +50,7 @@ export default defineConfig({
   outDir: "build",
 
   manifest: {
-    version: "1.3.1",
+    version: "1.3.2",
     name:
       process.env.TARGET === "firefox"
         ? "Page Assist - A Web UI for Local AI Models"
