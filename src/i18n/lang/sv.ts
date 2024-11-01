1564750852918bac2e6b775f3f8defaca0d68bd9
Pierre Mesure <pierre@mesu.re>
2024-11-01 17:45:08 +0100

    Add Swedish translation

---
+++ src/i18n/lang/sv.ts
@@ -0,0 +1,19 @@
+import option from "@/assets/locale/sv/option.json";
+import playground from "@/assets/locale/sv/playground.json";
+import common from "@/assets/locale/sv/common.json";
+import sidepanel from "@/assets/locale/sv/sidepanel.json";
+import settings from "@/assets/locale/sv/settings.json";
+import knowledge from "@/assets/locale/sv/knowledge.json";
+import chrome from "@/assets/locale/sv/chrome.json";
+import openai from "@/assets/locale/sv/openai.json";
+
+export const sv = {
+    option,
+    playground,
+    common,
+    sidepanel,
+    settings,
+    knowledge,
+    chrome,
+    openai
+}
