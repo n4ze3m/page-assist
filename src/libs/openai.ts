af897430395a42265b23f5973da363fc2e71b36f
yz778 <ying17@gmail.com>
2025-01-28 21:27:05 -0800

    fix: alternate handler for retrieving Gemini models

--- src/libs/openai.ts
+++ src/libs/openai.ts
@@ -2,6 +2,7 @@ type Model = {
   id: string
   name?: string
   display_name?: string
+  displayName?: string
   type: string
 }
 
@@ -23,6 +24,20 @@ export const getAllOpenAIModels = async (baseUrl: string, apiKey?: string) => {
     })
 
     clearTimeout(timeoutId)
+
+    // if Gemini fails to return models, try another approach
+    if (res.status === 401 && res.url == 'https://generativelanguage.googleapis.com/v1beta/openai/models') {
+      const urlGoogle = `https://generativelanguage.googleapis.com/v1beta/models?key=${apiKey}`
+      const resGoogle = await fetch(urlGoogle, {
+        signal: controller.signal
+      })
+
+      const data = await resGoogle.json()
+      return data.models.map(model => ({
+        id: model.name.replace(/^models\//, ""),
+        name: model.displayName,
+      })) as Model[]
+    }
 
     if (!res.ok) {
       return []
