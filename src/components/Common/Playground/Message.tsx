342d544e30fd5f331ba7d49742ba5938a03404b3
n4ze3m <n4ze3m@gmail.com>
2025-02-01 11:22:12 +0530

    feat: Add Baidu search engine

--- src/components/Common/Playground/Message.tsx
+++ src/components/Common/Playground/Message.tsx
@@ -54,7 +54,7 @@ export const PlaygroundMessage = (props: Props) => {
   return (
     <div className="group w-full text-gray-800 dark:text-gray-100">
       <div className="text-base md:max-w-2xl lg:max-w-xl xl:max-w-3xl flex lg:px-0 m-auto w-full">
-        <div className="flex flex-row gap-4 md:gap-6 p-4 md:py-6 lg:px-0 m-auto w-full">
+        <div className="flex flex-row gap-4 md:gap-6 p-4 m-auto w-full">
           <div className="w-8 flex flex-col relative items-end">
             <div className="relative h-7 w-7 p-1 rounded-sm text-white flex items-center justify-center  text-opacity-100r">
               {props.isBot ? (
@@ -150,7 +150,6 @@ export const PlaygroundMessage = (props: Props) => {
             </div>
             {/* source if available */}
             {props.images &&
-              props.images &&
               props.images.filter((img) => img.length > 0).length > 0 && (
                 <div className="flex md:max-w-2xl lg:max-w-xl xl:max-w-3xl mt-4 m-auto w-full">
                   {props.images
