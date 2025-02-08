87c2024771991dfcc50c8d342cb2522607ea81e9
n4ze3m <n4ze3m@gmail.com>
2025-02-08 21:18:09 +0530

    chore: update version to 1.5.0 in wxt.config.ts

--- wxt.config.ts
+++ wxt.config.ts
@@ -51,7 +51,7 @@ export default defineConfig({
   outDir: "build",
 
   manifest: {
-    version: "1.4.6",
+    version: "1.5.0",
     name:
       process.env.TARGET === "firefox"
         ? "Page Assist - A Web UI for Local AI Models"
