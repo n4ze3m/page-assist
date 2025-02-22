3c2219c0cfdcebae8d08c552578d273990876982
n4ze3m <n4ze3m@gmail.com>
2025-02-22 10:19:51 +0530

    chore: bump version to 1.5.2 in wxt.config.ts

--- wxt.config.ts
+++ wxt.config.ts
@@ -51,7 +51,7 @@ export default defineConfig({
   outDir: "build",
 
   manifest: {
-    version: "1.5.1",
+    version: "1.5.2",
     name:
       process.env.TARGET === "firefox"
         ? "Page Assist - A Web UI for Local AI Models"
