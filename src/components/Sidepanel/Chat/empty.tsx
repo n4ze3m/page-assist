c4da9a59cab636fa2b500da60236984abea70c75
kostiantyn-kugot <kostiantyn.kugot@oracle.com>
2026-01-26 17:12:10 +0100

    Fix to the "Chat with curren tpage" checkbox in sidebar

--- src/components/Sidepanel/Chat/empty.tsx
+++ src/components/Sidepanel/Chat/empty.tsx
@@ -106,7 +106,7 @@ export const EmptySidePanel = () => {
                 onChange={(e) => {
                   setChatMode(e.target.checked ? "rag" : "normal")
                 }}
-                className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity"
+                className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all checked:border-blue-600 checked:bg-blue-600 before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity"
                 id="check"
               />
               <span className="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100 ">
