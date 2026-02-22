251deedae7a228e4d47349136df36967d15c03a5
Muhammed Nazeem <n4ze3m@gmail.com>
2026-02-22 18:56:38 +0530

    chore: update version to 1.5.54 in manifest

--- wxt.config.ts
+++ wxt.config.ts
@@ -51,7 +51,7 @@ export default defineConfig({
   outDir: "build",
 
   manifest: {
-    version: "1.5.53",
+    version: "1.5.54",
     name:
       process.env.TARGET === "firefox"
         ? "Page Assist - A Web UI for Local AI Models"
