7bab2d0351c001e324fdaa3fe1c0571f61495a3e
Muhammed Nazeem <n4ze3m@gmail.com>
2026-03-28 12:25:53 +0530

    feat: bump version to 1.5.59 in manifest

--- wxt.config.ts
+++ wxt.config.ts
@@ -51,7 +51,7 @@ export default defineConfig({
   outDir: "build",
 
   manifest: {
-    version: "1.5.58",
+    version: "1.5.59",
     name:
       process.env.TARGET === "firefox"
         ? "Page Assist - A Web UI for Local AI Models"
