a31356d3f5144326d1fe364d18d6310f18574fe7
Muhammed Nazeem <n4ze3m@gmail.com>
2026-06-21 20:41:16 +0530

    feat: add new externally connectable ID to Page Action configuration

--- extensions/page-action/wxt.config.ts
+++ extensions/page-action/wxt.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
         'jfgfiigpkhlkbnfnbobbkinehhfdhndo',
         'ogkogooadflifpmmidmhjedogicnhooa',
         'ijokhblbfikokhbbfnndfajaaanmmjdg',
+        'mdegghmhjneppdkhefiedfeldioplioo'
       ],
     },
     action: {
